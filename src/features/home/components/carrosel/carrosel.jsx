72eaa6ce7a0a9182408e07cb0f7315592d6cce9c
saraiva142 <saraivajandrade@gmail.com>
2025-05-14 20:28:32 -0300

    Home ok e organização de pastas eu acho q tbm 🏠

--- src/features/home/components/carrosel/carrosel.jsx
+++ src/features/home/components/carrosel/carrosel.jsx
@@ -4,7 +4,7 @@ export default function Carrosel() {
         <div id="carouselExampleInterval" class="carousel slide" data-bs-ride="carousel">
             <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="4000">
-                <img src="/cartazes/rago.png" class="d-block w-100" alt="..."></img>
+                <img src="/cartazes/walter-cover.png" class="d-block w-100" alt="..."></img>
             </div>
             <div class="carousel-item" data-bs-interval="2000">
                 <img src="/cartazes/rago.png" class="d-block w-100" alt="..."></img>
--- src/features/home/pages/Home.jsx
+++ src/features/home/pages/Home.jsx
@@ -6,6 +6,24 @@ export default function Home() {
         <>
         <Menu />
         <Carrosel />
+        <div className="container mt-5">
+            <h1 className="text-center" style={{fontWeight: "bolder"}}>Bem-vindo ao CineSaraiva🎬</h1>
+            <p className="text-center">Aqui você pode encontrar os melhores filmes e sessões disponíveis.</p>
+            <div className="row" style={{display: "flex", textAlign: "center"}}>
+                <div className="col-md-4">
+                    <h2>Filmes em Cartaz</h2>
+                    <p>Confira os filmes que estão em cartaz no CineSaraiva.</p>
+                </div>
+                <div className="col-md-4">
+                    <h2>Salas</h2>
+                    <p>Conheça nossas salas de cinema e suas capacidades.</p>
+                </div>
+                <div className="col-md-4">
+                    <h2>Sessões</h2>
+                    <p>Veja os horários das sessões disponíveis.</p>
+                </div>
+            </div>
+        </div>
         </>
     );
 }
